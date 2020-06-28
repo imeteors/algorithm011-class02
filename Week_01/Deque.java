4e4c50969ef2468d513a209f91b0fadb0dd06a8c
imeteors <imeteors@126.com>
2020-06-28 23:36:22 +0800

    commit homework

---
+++ Week_01/Deque.java
@@ -0,0 +1,18 @@
+package Week_01;
+
+import java.util.ArrayDeque;
+
+public class Deque {
+
+    public static void main(String[] args) {
+        java.util.Deque<String> deque = new ArrayDeque<>();
+        deque.addFirst("a");
+        deque.addFirst("b");
+        deque.addLast("c");
+        System.out.println(deque.peek());
+        while (!deque.isEmpty()) {
+            System.out.println(deque.removeFirst());
+        }
+    }
+    
+}
